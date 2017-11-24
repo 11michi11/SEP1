c66aa57c9367271a97149f812a619e01fe61c010
11michi11 <11michi11@gmail.com>
2017-11-24 11:39:38 +0100

    Merge

---
+++ RWD3/js/script.js
@@ -0,0 +1,87 @@
+addPoping("#bubble1Id");
+addPoping("#bubble2Id");
+addPoping("#bubble3Id");
+
+animateBubble("#bubble1Id");
+animateBubble("#bubble2Id");
+animateBubble("#bubble3Id");
+
+function animateBubble(bubbleId) {
+    initBubble(bubbleId);
+
+    var speed = Math.floor(Math.random() * 6) + 4;
+
+    $(bubbleId).animate({
+        top: -100
+    }, (speed * 1000), function () {
+        animateBubble(bubbleId);
+    });
+}
+
+function initBubble(bubbleId) {
+    $(bubbleId).show();
+    var top = $(window).height();
+    var left = getRandX();
+    $(bubbleId).offset({
+        top: top,
+        left: left
+    });
+}
+
+function getRandX() {
+    var screenWidth = $(window).width();
+    var imgWidth = 100;
+    var maxVal = screenWidth - imgWidth;
+    console.log(maxVal);
+    return Math.floor(Math.random() * maxVal);
+}
+
+function addPoping(bubbleId) {
+    $(bubbleId).click(function () {
+        $(this).stop(true);
+        $(this).fadeOut(function () {
+            animateBubble(bubbleId);
+        });
+    });
+}
+
+
+swim("#fish1Id");
+swim("#fish2Id");
+
+$("#fish2Id").on("mouseenter", function () {
+    var possibleWidth = $(window).width() - $("#fish2Id").width();
+    var possibleHieght = $(window).height() - $("#fish2Id").height();
+
+    var x = Math.floor(Math.random() * possibleWidth);
+    var y = Math.floor(Math.random() * possibleHieght);
+
+    $("#fish2Id").stop(true).animate({ left: x, top: y }, "fast", function () {
+        swim("#fish2Id");
+    });
+});
+
+function randomX(IdRef) {
+    var x;
+    x = Math.floor(Math.random() * ($(window).width() - $(IdRef).width()));
+    return x;
+}
+
+function randomY(IdRef) {
+    var y;
+    y = Math.floor(Math.random() * ($(window).height() - $(IdRef).height()));
+    return y;
+}
+function swim(IdRef) {
+    var x, y, v, moveX, moveY;
+    x = randomX(IdRef);
+    y = randomY(IdRef);
+    moveX = x - $(IdRef).offset().left;
+    moveY = y - $(IdRef).offset().top;
+
+    if (IdRef === "#fish1Id")
+        v = Math.sqrt(Math.pow(moveX, 2) + Math.pow(moveY, 2)) * 3;
+    else
+        v = Math.sqrt(Math.pow(moveX, 2) + Math.pow(moveY, 2)) * 17;
+    $(IdRef).animate({ left: x, top: y }, v, "linear", function () { swim(IdRef); });
+}
