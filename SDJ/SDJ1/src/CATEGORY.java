8b7e555153d5c5e64bb939eceead29a430a7bd7b
11michi11 <11michi11@gmail.com>
2017-11-30 14:21:07 +0100

    Small changes everywhere

--- SDJ/SDJ1/src/CATEGORY.java
+++ SDJ/SDJ1/src/CATEGORY.java
@@ -1,24 +1,32 @@
-import java.util.ArrayList;
 
-public class CATEGORY {
-   private ArrayList<String> categoryList;
-   
-   public CATEGORY()
-   {
-      categoryList=new ArrayList<String>();
-   }
-   public void addCategory(String category)
-   {
-      categoryList.add(category);
-   }
-   public String findCategory(String category)
-   {
-      for(String i:categoryList)
-      {
-         if(i.equals(category))
-            return i;
-      }
-      return "No such category";
-   }
+public enum CATEGORY {
+	
+	Astrology,
+	Meditation,
+	Reincarnation,
+	Health,
+	TraditionalBuddhism,
+	Nature,
+	Other
+	
+//   private ArrayList<String> categoryList;
+//   
+//   public CATEGORY()
+//   {
+//      categoryList=new ArrayList<String>();
+//   }
+//   public void addCategory(String category)
+//   {
+//      categoryList.add(category);
+//   }
+//   public String findCategory(String category)
+//   {
+//      for(String i:categoryList)
+//      {
+//         if(i.equals(category))
+//            return i;
+//      }
+//      return "No such category";
+//   }
    
 }
