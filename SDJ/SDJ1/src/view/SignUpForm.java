4e8915ec152543801695e745e9626dcd1e72328c
11michi11 <11michi11@gmail.com>
2017-12-01 10:17:35 +0100

    Format

--- SDJ/SDJ1/src/view/SignUpForm.java
+++ SDJ/SDJ1/src/view/SignUpForm.java
@@ -9,145 +9,136 @@ import javax.swing.JTextField;
 import java.awt.EventQueue;
 import java.awt.GridLayout;
 
-public class SignUpForm extends JFrame
-{
+public class SignUpForm extends JFrame {
 
-   private JLabel labelSignUpForm;
-   private JLabel labelName;
-   private JLabel labelEmail;
-   private JLabel labelAddress;
-   private JLabel labelID;
-   private JLabel labelPhone;
-   private JLabel labelDateOfMembership;
-   private JLabel labelPaymentYear;
-   private JTextField textFieldName;
-   private JTextField textFieldEmail;
-   private JTextField textFieldAddress;
-   private JTextField textFieldID;
-   private JTextField textFieldPhone;
-   private JTextField textFieldDateOfMembership;
-   private JTextField textFieldPaymentYear;
-   private JButton AddToList;
-   private JPanel name;
-   private JPanel email;
-   private JPanel address;
-   private JPanel id;
-   private JPanel phone;
-   private JPanel dateOfMembership;
-   private JPanel paymentYear;
+	private JLabel labelSignUpForm;
+	private JLabel labelName;
+	private JLabel labelEmail;
+	private JLabel labelAddress;
+	private JLabel labelID;
+	private JLabel labelPhone;
+	private JLabel labelDateOfMembership;
+	private JLabel labelPaymentYear;
+	private JTextField textFieldName;
+	private JTextField textFieldEmail;
+	private JTextField textFieldAddress;
+	private JTextField textFieldID;
+	private JTextField textFieldPhone;
+	private JTextField textFieldDateOfMembership;
+	private JTextField textFieldPaymentYear;
+	private JButton AddToList;
+	private JPanel name;
+	private JPanel email;
+	private JPanel address;
+	private JPanel id;
+	private JPanel phone;
+	private JPanel dateOfMembership;
+	private JPanel paymentYear;
 
-   public SignUpForm()
-   {
+	public SignUpForm() {
 
-      super("SignUpForm");
-      createComponents();
-      initializeComponents();
-      registerEventHandlers();
-      addComponentsToFrame();
+		super("SignUpForm");
+		createComponents();
+		initializeComponents();
+		registerEventHandlers();
+		addComponentsToFrame();
 
-   }
+	}
 
-   public void createComponents()
-   {
+	public void createComponents() {
 
-      labelSignUpForm = new JLabel("Sign-Up Form");
-      labelName = new JLabel("Name:");
-      labelEmail = new JLabel("E-mail:");
-      labelAddress = new JLabel("Address:");
-      labelID = new JLabel("ID:");
-      labelPhone = new JLabel("Phone:");
-      labelDateOfMembership = new JLabel("Date of membership:");
-      labelPaymentYear = new JLabel("Payment year:");
-      textFieldName = new JTextField(8);
-      textFieldEmail = new JTextField(8);
-      textFieldAddress = new JTextField(8);
-      textFieldID = new JTextField(8);
-      textFieldPhone = new JTextField(8);
-      textFieldDateOfMembership = new JTextField(8);
-      textFieldPaymentYear = new JTextField(8);
-      AddToList = new JButton("Add to list");
+		labelSignUpForm = new JLabel("Sign-Up Form");
+		labelName = new JLabel("Name:");
+		labelEmail = new JLabel("E-mail:");
+		labelAddress = new JLabel("Address:");
+		labelID = new JLabel("ID:");
+		labelPhone = new JLabel("Phone:");
+		labelDateOfMembership = new JLabel("Date of membership:");
+		labelPaymentYear = new JLabel("Payment year:");
+		textFieldName = new JTextField(8);
+		textFieldEmail = new JTextField(8);
+		textFieldAddress = new JTextField(8);
+		textFieldID = new JTextField(8);
+		textFieldPhone = new JTextField(8);
+		textFieldDateOfMembership = new JTextField(8);
+		textFieldPaymentYear = new JTextField(8);
+		AddToList = new JButton("Add to list");
 
-   }
+	}
 
-   public void initializeComponents()
-   {
+	public void initializeComponents() {
 
-      setSize(500, 500);
-      setLocationRelativeTo(null);
-      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(500, 500);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-   }
+	}
 
-   public void registerEventHandlers()
-   {
+	public void registerEventHandlers() {
 
-   }
+	}
 
-   public void addComponentsToFrame()
-   {
-      JPanel pane = new JPanel();
+	public void addComponentsToFrame() {
+		JPanel pane = new JPanel();
 
-      JPanel left = new JPanel(new GridLayout(4, 1));
+		JPanel left = new JPanel(new GridLayout(4, 1));
 
-      name = new JPanel();
-      name.add(labelName);
-      name.add(textFieldName);
+		name = new JPanel();
+		name.add(labelName);
+		name.add(textFieldName);
 
-      email = new JPanel();
-      email.add(labelEmail);
-      email.add(textFieldEmail);
+		email = new JPanel();
+		email.add(labelEmail);
+		email.add(textFieldEmail);
 
-      address = new JPanel();
-      address.add(labelAddress);
-      address.add(textFieldAddress);
+		address = new JPanel();
+		address.add(labelAddress);
+		address.add(textFieldAddress);
 
-      id = new JPanel();
-      id.add(labelID);
-      id.add(textFieldID);
+		id = new JPanel();
+		id.add(labelID);
+		id.add(textFieldID);
 
-      left.add(name);
-      left.add(email);
-      left.add(address);
-      left.add(id);
+		left.add(name);
+		left.add(email);
+		left.add(address);
+		left.add(id);
 
-      JPanel right = new JPanel(new GridLayout(3, 1));
+		JPanel right = new JPanel(new GridLayout(3, 1));
 
-      phone = new JPanel();
-      phone.add(labelPhone);
-      phone.add(textFieldPhone);
+		phone = new JPanel();
+		phone.add(labelPhone);
+		phone.add(textFieldPhone);
 
-      dateOfMembership = new JPanel();
-      dateOfMembership.add(labelDateOfMembership);
-      dateOfMembership.add(textFieldDateOfMembership);
+		dateOfMembership = new JPanel();
+		dateOfMembership.add(labelDateOfMembership);
+		dateOfMembership.add(textFieldDateOfMembership);
 
-      paymentYear = new JPanel();
-      paymentYear.add(labelPaymentYear);
-      paymentYear.add(textFieldPaymentYear);
+		paymentYear = new JPanel();
+		paymentYear.add(labelPaymentYear);
+		paymentYear.add(textFieldPaymentYear);
 
-      right.add(phone);
-      right.add(dateOfMembership);
-      right.add(paymentYear);
+		right.add(phone);
+		right.add(dateOfMembership);
+		right.add(paymentYear);
 
-      pane.add(left);
-      pane.add(right);
+		pane.add(left);
+		pane.add(right);
 
-      setContentPane(pane);
+		setContentPane(pane);
 
-   }
+	}
 
-   public static void main(String[] args)
-   {
+	public static void main(String[] args) {
 
-      EventQueue.invokeLater(new Runnable()
-      {
-         @Override
-         public void run()
-         {
-            SignUpForm main = new SignUpForm();
-            main.setVisible(true);
-         }
-      });
+		EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				SignUpForm main = new SignUpForm();
+				main.setVisible(true);
+			}
+		});
 
-   }
+	}
 
 }
