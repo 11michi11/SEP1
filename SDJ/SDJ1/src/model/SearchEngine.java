79592db43bf3fbb3177687f8e73a36d4a3d45e96
11michi11 <11michi11@gmail.com>
2017-12-13 13:21:28 +0100

    Formatting

--- SDJ/SDJ1/src/model/SearchEngine.java
+++ SDJ/SDJ1/src/model/SearchEngine.java
@@ -6,54 +6,53 @@ import java.util.regex.Pattern;
 
 public class SearchEngine {
 
-    public static ArrayList<Member> searchForMembers(ArrayList<Member> members, String needle) {
-	
-	ArrayList<Member> searchedMembers = new ArrayList<Member>();
-	for (Member e : members) {
-	    if (search(e.getName(), needle))
-		searchedMembers.add(e);
-	    else if(search(Integer.toString(e.getID()), needle))
-		searchedMembers.add(e);
+	public static ArrayList<Member> searchForMembers(ArrayList<Member> members, String needle) {
+
+		ArrayList<Member> searchedMembers = new ArrayList<Member>();
+		for (Member e : members) {
+			if (search(e.getName(), needle))
+				searchedMembers.add(e);
+			else if (search(Integer.toString(e.getID()), needle))
+				searchedMembers.add(e);
+		}
+
+		return searchedMembers;
 	}
 
-	return searchedMembers;
-    }
+	public static ArrayList<Lecturer> searchForLecturers(ArrayList<Lecturer> lecturers, String needle) {
+		ArrayList<Lecturer> searchedLecturers = new ArrayList<Lecturer>();
+		for (Lecturer e : lecturers) {
+			if (search(e.getName(), needle) || search(Integer.toString(e.getID()), needle))
+				searchedLecturers.add(e);
+		}
+		return searchedLecturers;
+	}
 
-    public static ArrayList<Lecturer> searchForLecturers(ArrayList<Lecturer> lecturers, String needle) {
-	ArrayList<Lecturer> searchedLecturers = new ArrayList<Lecturer>();
-	for (Lecturer e: lecturers) {
-	    if (search(e.getName(), needle) || search(Integer.toString(e.getID()), needle))
-		searchedLecturers.add(e);
+	public static ArrayList<Event> searchForEvents(ArrayList<Event> events, String needle) {
+		ArrayList<Event> searchedEvents = new ArrayList<Event>();
+		for (Event e : events) {
+			if (search(e.getTitle(), needle) || search(Integer.toString(e.getID()), needle)
+					|| search(e.getStartDate().toString(), needle))
+				searchedEvents.add(e);
+		}
+		return searchedEvents;
 	}
-	return searchedLecturers;
-    }
 
-    public static ArrayList<Event> searchForEvents(ArrayList<Event> events, String needle) {
-	ArrayList<Event> searchedEvents = new ArrayList<Event>();
-	for (Event e: events) {
-	    if (search(e.getTitle(), needle) || search(Integer.toString(e.getID()), needle)
-		    || search(e.getStartDate().toString(), needle))
-		searchedEvents.add(e);
+	public static boolean search(String stack, String needle) {
+		Pattern p = Pattern.compile(stack);
+		Matcher m = p.matcher("");
+		m.reset(needle);
+		System.out.println(needle + " in " + stack);
+		if (m.matches() || m.hitEnd()) {
+			System.out.println("case 1");
+			return true;
+		} else if (stack.length() > needle.length()) {
+			String newStack = stack.substring(1, stack.length());
+			System.out.println("case 2");
+			return search(newStack, needle);
+		} else {
+			System.out.println("case 3");
+			return false;
+		}
 	}
-	return searchedEvents;
-    }
-
-    public static boolean search(String stack, String needle) {
-	Pattern p = Pattern.compile(stack);
-	Matcher m = p.matcher("");
-	m.reset(needle);
-	System.out.println(needle+" in "+stack);
-	if (m.matches() || m.hitEnd()) {
-	    System.out.println("case 1");
-	    return true;
-	}
-	else if (stack.length() > needle.length()) {
-	    String newStack = stack.substring(1,stack.length());
-	    System.out.println("case 2");
-	    return search(newStack, needle);
-	} else {
-	    System.out.println("case 3");
-	    return false;
-	}
-    }
 }
