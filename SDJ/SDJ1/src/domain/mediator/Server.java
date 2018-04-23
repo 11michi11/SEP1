3fd2e892df9618e352f75472cb64cbccd753f05d
Miska <michaela.golhova@gmail.com>
2018-04-23 15:55:23 +0200

    RMI server/client

--- SDJ/SDJ1/src/domain/mediator/Server.java
+++ SDJ/SDJ1/src/domain/mediator/Server.java
@@ -1,5 +1,55 @@
 package domain.mediator;
 
-public class Server {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.RemoteServer;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+import java.util.Map;
 
-}
+import domain.model.Category;
+import domain.model.Event;
+import domain.model.EventList;
+import domain.model.EventNotFoundException;
+import domain.model.Lecturer;
+import domain.model.LecturerList;
+import domain.model.Member;
+import domain.model.MemberList;
+import domain.model.MyDate;
+import domain.model.Participant;
+
+public class Server extends UnicastRemoteObject implements ServerManager {
+
+	private static final long serialVersionUID = 1L;
+	private VIAManager manager;
+	
+	protected Server() throws RemoteException {
+		super();
+		manager = new VIAManager();
+	}
+	
+	public void startServer() {
+		try {
+			Registry registry = LocateRegistry.createRegistry(1099);
+			RemoteServer rmi = new Server();
+			Naming.rebind("server", rmi);
+			System.out.println("Server is up");
+			
+		} catch (RemoteException | MalformedURLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	@Override
+	public ArrayList<Member> getAllMembers() throws RemoteException {
+		return manager.getAllMembers();
+	}
+
+	}
---
+++ SDJ/SDJ1/src/domain/mediator/ServerManager.java
@@ -0,0 +1,13 @@
+package domain.mediator;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
+import domain.model.Member;
+
+public interface ServerManager extends Remote {
+	
+	public ArrayList<Member> getAllMembers() throws RemoteException;
+
+}
